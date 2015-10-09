f53e7efe2fe9fcb5d507470a2a1b9cdf47a7793e
Alex Vermeer <alex.vrmr@gmail.com>
2015-10-09 14:16:58 +0200

    Inferitance fix
    Hopefully...

--- Tetris/Tetris/TetrisBlock.cs
+++ Tetris/Tetris/TetrisBlock.cs
@@ -4,10 +4,15 @@ namespace Practicum.Tetris
 {
     class TetrisBlock : GridObject
     {
-        public sbyte offsetX, offsetY;
-        private static int prevBlock;
+        private sbyte offsetX, offsetY;
+        public sbyte OffsetX { get { return offsetX; } }
+        public sbyte OffsetY { get { return offsetY; } }
 
-        public static TetrisBlock createBlock(int blockKind = -1, bool isColor=false)
+        private byte color;
+        public byte Color { get { return color; } }
+
+        private static int prevBlock;
+        public static TetrisBlock createBlock(int blockKind = -1, byte color = 0)
         {
             TetrisBlock block;
 
@@ -24,41 +29,42 @@ namespace Practicum.Tetris
             switch (blockKind)
                 {
                     case 0:
-                        //square
-                        block = new blockSquare(isColor);
+                    //square
+                        block = color == 0 ? new blockSquare() : new blockSquare(color);
                         break;
                     case 1:
                         //horizontal line
-                        block = new blockLineH(isColor);
+                        block = color == 0 ? new blockLineH() : new blockLineH(color);
                         break;
                     case 2:
                         //vertical line
-                        block = new blockLineV(isColor);
+                        block = color == 0 ? new blockLineV() : new blockLineV(color);
                         break;
                     case 3:
-                        block = new blockZ(isColor);
+                        block = color == 0 ? new blockZ() : new blockZ(color);
                         break;
                     case 4:
-                        block = new blockReverseZ(isColor);
+                        block = color == 0 ? new blockReverseZ() : new blockReverseZ(color);
                         break;
                     case 5:
-                        block = new blockFlatL(isColor);
+                        block = color == 0 ? new blockFlatL() : new blockFlatL(color);
                         break;
                     case 6:
-                        block = new blockFlatReverseL(isColor);
+                        block = color == 0 ? new blockFlatReverseL() : new blockFlatReverseL(color);
                         break;
                     case 7:
-                        block = new blockC(isColor);
+                        block = color == 0 ? new blockC() : new blockC(color);
                         break;
                     case 8:
-                        block = new blockRoof(isColor);
+                        block = color == 0 ? new blockRoof() : new blockRoof(color);
                         break;
                     
                     
                     //alle andere blokken hier ook toevoegen... (case 3-case 9)
                     default:
                         //default is square dus als er iets mis gaat wordt t een vierkant
-                        block = new Block(false, false, false, false, false, true, true, false, false, true, true, false, false, false, false, false, isColor);
+                        block = color == 0 ? new TetrisBlock(false, false, false, false, false, true, true, false, false, true, true, false, false, false, false, false)
+                                           : new TetrisBlock(false, false, false, false, false, true, true, false, false, true, true, false, false, false, false, false, color);
                         break;
                     
                 }
@@ -72,40 +78,12 @@ namespace Practicum.Tetris
             fillStruct(x1y1, x2y1, x3y1, x4y1, x1y2, x2y2, x3y2, x4y2, x1y3, x2y3, x3y3, x4y3, x1y4, x2y4, x3y4, x4y4);
         }
 
-        /// <summary>
-        /// Creates a new tetris block structure wich may allow color differences.
-        /// </summary>
-        /// <param name="x1y1">Is there a block at the internal x = 1, y = 1 coördinate?</param>
-        /// <param name="x2y1">Is there a block at the internal x = 2, y = 1 coördinate?</param>
-        /// <param name="x3y1">Is there a block at the internal x = 3, y = 1 coördinate?</param>
-        /// <param name="x4y1">Is there a block at the internal x = 4, y = 1 coördinate?</param>
-        /// <param name="x1y2">Is there a block at the internal x = 1, y = 2 coördinate?</param>
-        /// <param name="x2y2">Is there a block at the internal x = 2, y = 2 coördinate?</param>
-        /// <param name="x3y2">Is there a block at the internal x = 3, y = 2 coördinate?</param>
-        /// <param name="x4y2">Is there a block at the internal x = 4, y = 2 coördinate?</param>
-        /// <param name="x1y3">Is there a block at the internal x = 1, y = 3 coördinate?</param>
-        /// <param name="x2y3">Is there a block at the internal x = 2, y = 3 coördinate?</param>
-        /// <param name="x3y3">Is there a block at the internal x = 3, y = 3 coördinate?</param>
-        /// <param name="x4y3">Is there a block at the internal x = 4, y = 3 coördinate?</param>
-        /// <param name="x1y4">Is there a block at the internal x = 1, y = 4 coördinate?</param>
-        /// <param name="x2y4">Is there a block at the internal x = 2, y = 4 coördinate?</param>
-        /// <param name="x3y4">Is there a block at the internal x = 3, y = 4 coördinate?</param>
-        /// <param name="x4y4">Is there a block at the internal x = 4, y = 4 coördinate?</param>
-        /// <param name="color">The color of the block.</param>
-        public Block(bool x1y1, bool x2y1, bool x3y1, bool x4y1, bool x1y2, bool x2y2, bool x3y2, bool x4y2, bool x1y3, bool x2y3, bool x3y3, bool x4y3, bool x1y4, bool x2y4, bool x3y4, bool x4y4, byte color)
-            : this(x1y1, x2y1, x3y1, x4y1, x1y2, x2y2, x3y2, x4y2, x1y3, x2y3, x3y3, x4y3, x1y4, x2y4, x3y4, x4y4, true)
+        public TetrisBlock(bool x1y1, bool x2y1, bool x3y1, bool x4y1, bool x1y2, bool x2y2, bool x3y2, bool x4y2, bool x1y3, bool x2y3, bool x3y3, bool x4y3, bool x1y4, bool x2y4, bool x3y4, bool x4y4, byte color) : base(4, 4, true)
         {
-
             this.color = color;
 
-            for(int i = 0; i < blockStruc.Length; i++)
-            {
-                for(int j = 0; j<blockStruc[i].Length; j++)
-                {
-                    if (blockStruc[i][j]) { blockCol[i][j] = color; }
-                    else { blockCol[i][j] = 0; }
-                }
-            }
+            fillStruct(x1y1, x2y1, x3y1, x4y1, x1y2, x2y2, x3y2, x4y2, x1y3, x2y3, x3y3, x4y3, x1y4, x2y4, x3y4, x4y4);
+            colorBlock(color);
         }
 
         public void moveDown()
@@ -166,5 +144,16 @@ namespace Practicum.Tetris
             fieldStruc[0][3] = x1y4; fieldStruc[1][3] = x2y4; fieldStruc[2][3] = x3y4; fieldStruc[3][3] = x4y4;
         }
 
+        private void colorBlock(byte color)
+        {
+            for (int i = 0; i < fieldStruc.Length; i++)
+            {
+                for (int j = 0; j < fieldStruc[i].Length; j++)
+                {
+                    if (fieldStruc[i][j]) { fieldCol[i][j] = color; }
+                    else { fieldCol[i][j] = 0; }
+                }
+            }
+        }
     }
 }
