25b5b32b9f10522928fa3b5a72079677236467ba
SombreroZonderC <Rik.devendt@gmail.com>
2015-10-09 11:37:37 +0200

    All block constructors
    Added subclasses and constructors for every kind of block. Instead of
    needing to call the Block constructor, instead call the kind of block
    and it will automatically create the right kind of block.

---
+++ Tetris/Tetris/blockSquare.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practicum.Tetris
+{
+    class blockSquare:Block
+    {
+       
+        public blockSquare(bool isColor = false)
+            : base(false, false, false, false, false, true, true, false, false, true, true, false, false, false, false, false, isColor) 
+        {
+        
+        }
+    }
+    class blockLineH:Block
+    {
+        
+        public blockLineH(bool isColor = false)
+            : base(false, false, false, false, false, false, false, false, true, true, true, true, false, false, false, false, isColor) 
+        {
+        
+        }
+
+    }
+    class blockLineV:Block
+    {
+
+        public blockLineV(bool isColor = false)
+            : base(false, true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, isColor) 
+        {
+        
+        }
+    }
+    class blockZ:Block
+    {
+    
+        public blockZ(bool isColor = false)
+            : base(false, false, false, false, false, true, true, false, false, false, true, true, false, false, false, false, isColor) 
+        {
+        
+        }
+    }
+    class blockReverseZ : Block
+    {
+        public blockReverseZ(bool isColor = false)
+            : base(false, false, false, false, false, false, true, true, false, true, true, false, false, false, false, false, isColor)
+        {
+
+        }
+
+    }
+    class blockFlatReverseL:Block
+    {
+
+        public blockFlatReverseL(bool isColor = false)
+            : base(false, false, false, false, false, true, false, false, false, true, true, true, false, false, false, false, isColor) 
+        {
+        
+        }
+
+    }
+    class blockFlatL:Block
+    {
+        public blockFlatL(bool isColor = false)
+            : base(false, false, false, false, false, false, true, false, true, true, true, false, false, false, false, false, isColor) 
+        {
+        
+        }
+    }
+    class blockC:Block
+    {
+        public blockC(bool isColor = false)
+            : base(false, false, false, false, false, true, true, false, false, false, true, false, false, false, false, false, isColor) 
+        {
+        
+        }
+    
+    }
+    class blockRoof:Block
+    {
+        public blockRoof(bool isColor = false)
+            : base(false, false, false, false, false, false, true, false, false, true, true, true, false, false, false, false, isColor) 
+        {
+        
+        }
+    }
+}
